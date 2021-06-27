fd8255659aa9d8b2876dcae2ba885abef3266bd4
Faisal Nazir <faisalnazir22@gmail.com>
2021-06-27 23:44:28 +0500

    Admin Dashboard Changed

--- UserTest/settings.py
+++ UserTest/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['103.156.244.253','localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlte3',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
